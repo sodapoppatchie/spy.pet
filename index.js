ca7caa08d10edcd95281aebba8073b0bd1cdc914
Lunar <161990111+LunarrBlue@users.noreply.github.com>
2024-05-15 20:57:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,38 +6,38 @@ const path = require("path");
 const secretsPath = path.join(__dirname, "secrets.json");
 const secrets = JSON.parse(fs.readFileSync(secretsPath));
 
-// Use the first bot token in secrets
-const tokenVar = secrets[Object.keys(secrets)[0]];
+// Select the first bot from the secrets
+const selfbotName = Object.keys(secrets)[0];
+const tokenVar = secrets[selfbotName];
+
+// Create a Client instance
 const client = new Client();
 
-let notify = 5000;
-let scannedMessages = 0;
-let totalScannedMessages = 0;
-
+// Client login
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.login(tokenVar);
 
-client.on("messageCreate", (message) => {
+// Message creation
+client.on("messageCreate", async (message) => {
     if (message.author.bot || !message.guild) return; // Ignore messages from bots and DMs
-    
-    // Log the message details to the console
-    console.log(`Author: ${message.author.username}`);
-    console.log(`Server: ${message.guild.name}`);
-    console.log(`Server ID: ${message.guild.id}`);
-    console.log(`Channel: ${message.channel.name}`);
-    console.log(`Channel ID: ${message.channel.id}`);
-    console.log(`NSFW Channel? ${message.channel.nsfw}`);
-    console.log(`Sent At: ${message.createdAt}`);
-    console.log(`Message: ${message.content}`);
-
-    scannedMessages += 1;
-    totalScannedMessages += 1;
-
-    if (scannedMessages == notify) {
-        console.log(`Saved ${totalScannedMessages} messages`);
-        scannedMessages = 0;
-    }
+    // Save the message to a text file
+    const userDir = path.join(__dirname, "Users", message.author.username);
+    const messagesDir = path.join(userDir, "Messages");
+    const guildDir = path.join(messagesDir, message.guild.name);
+    createFolders([userDir, messagesDir, guildDir]);
+    const filePath = path.join(guildDir, `${message.id}.txt`);
+    fs.writeFileSync(
+        filePath,
+        `Author: ${message.author.username}
+          Server: ${message.guild.name}
+          Server ID: ${message.guild.id}
+          Channel: ${message.channel.name}
+          Channel ID: ${message.channel.id}
+          NSFW Channel? ${message.channel.nsfw}
+          Sent At: ${message.createdAt}
+          Message: ${message.content}`,
+    );
 });
